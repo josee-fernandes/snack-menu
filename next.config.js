0a22130037e9ed845919270b38b57989cf339994
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2023-07-17 13:40:13 -0300

    Fix image domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["https://slapsburgers.com/"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
